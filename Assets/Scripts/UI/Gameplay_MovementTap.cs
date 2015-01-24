eab8938ee126d6a5630792f302f1353be3149b5e
kinifi <chrisfigueroa16@gmail.com>
2015-01-23 22:34:12 -0500

    Added touch and keyboard controls

---
+++ Assets/Scripts/UI/Gameplay_MovementTap.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gameplay_MovementTap : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            movePlayerLeft();
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            movePlayerRight();
+        }
+
+
+    }
+
+    void movePlayerLeft()
+    {
+        this.rigidbody.AddForce(new Vector3(-10, 0, 0));
+    }
+
+    void movePlayerRight()
+    {
+        this.rigidbody.AddForce(new Vector3(10, 0, 0));
+    }
+
+    #region touch Public Methods
+    public void MoveLeft()
+    {
+        movePlayerLeft();
+        Debug.Log("Move Left");
+    }
+
+    public void MoveRight()
+    {
+        movePlayerRight();
+        Debug.Log("Move Right");
+    }
+    #endregion
+
+
+}
