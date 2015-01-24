37faad8995274d8413219011a71fb772472a68ea
kinifi <chrisfigueroa16@gmail.com>
2015-01-24 18:49:55 -0500

    added scaling planet

--- Assets/Scripts/planetController.cs
+++ Assets/Scripts/planetController.cs
@@ -3,13 +3,63 @@ using System.Collections;
 
 public class planetController : MonoBehaviour {
 
+    public float countdownTimer = 2.0f;
+    private float secondaryTimer;
+
 	// Use this for initialization
 	void Start () {
-	
+
+        //set the secondary timer so we can reset later
+        setSecondaryTimer();
 	}
 	
+    /// <summary>
+    /// sets the secondary timer to the first timer so we can reset the value later
+    /// </summary>
+    private void setSecondaryTimer()
+    {
+        secondaryTimer = countdownTimer;
+    }
+
+    /// <summary>
+    /// Method that resets the timer after complete
+    /// </summary>
+    private void resetTimer()
+    {
+        countdownTimer = secondaryTimer;
+    }
+
 	// Update is called once per frame
 	void Update () {
+
+        //countdown the timer
+        countdownTimer -= Time.deltaTime;
 	
+        //check if the timer is zero. We are checking this exactly because thats a hassle
+        if(countdownTimer < 0)
+        {
+            TimerOnComplete();
+        }
+
 	}
+
+    /// <summary>
+    /// The method that is called when the timer reaches zero
+    /// </summary>
+    void TimerOnComplete()
+    {
+        resetTimer();
+        scaleUp();
+        Debug.Log("Timer Complete");
+    }
+
+    /// <summary>
+    /// Scale up the local object that this script is attached to by x2
+    /// </summary>
+    void scaleUp()
+    {
+        transform.localScale = new Vector2(transform.localScale.x + 1, transform.localScale.y + 1);
+        Debug.Log("Scaling Up Planet");
+    }
+
 }
