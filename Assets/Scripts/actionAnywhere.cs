f4d0d928c080f4e1f49a5cee9e2207c9b54ae3f5
kinifi <chrisfigueroa16@gmail.com>
2015-01-25 15:45:28 -0500

    Fixed highscores
    yay

--- Assets/Scripts/actionAnywhere.cs
+++ Assets/Scripts/actionAnywhere.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class actionAnywhere : MonoBehaviour {
 
+	public string levelName = "HighScores";
+
 	// Use this for initialization
 	void Start () {
 	
@@ -14,7 +16,7 @@ public class actionAnywhere : MonoBehaviour {
 		if (Input.touchCount > 0 || Input.anyKeyDown || Input.GetMouseButtonDown(0))
 		{
 			Debug.Log("Loading HighScore Scene");
-			Application.LoadLevel("HighScores");
+			Application.LoadLevel(levelName);
 
 		}
 
