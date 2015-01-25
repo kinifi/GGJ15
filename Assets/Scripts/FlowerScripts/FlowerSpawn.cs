cb2eaccdda1a988f9e3c64018e160833e3bd0fc0
Hollie Buckets <bucketsinmn@gmail.com>
2015-01-25 13:11:27 -0500

    FlowerBowl prefab
    setup flower bowl prefab

--- Assets/Scripts/FlowerScripts/FlowerSpawn.cs
+++ Assets/Scripts/FlowerScripts/FlowerSpawn.cs
@@ -10,12 +10,40 @@ public class FlowerSpawn : MonoBehaviour {
 	public Vector2 flowerRigidBodySpeed;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		secondarySpawn = timerBetweenSpawn;
+		flowerRigidBodySpeed = new Vector2(0.0f, 200.0f);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		timerBetweenSpawn -= Time.deltaTime;
+		
+		if(timerBetweenSpawn < 0)
+		{
+			timerBetweenSpawn = secondarySpawn;
+			TimerOnComplete();
+		}
+	}
+
+	void TimerOnComplete()
+	{
+		SpawnFlower();
+		//Debug.Log("Completed");
+	}
+
+	void SpawnFlower()
+	{
+		//set up instatiation position
+		Vector2 flowerSpawnPosition = new Vector2(Random.Range(-3, 3), transform.position.y);
+		flowerSpawnPosition.y = -5.0f;
+
+		GameObject _flower;
+		_flower = Instantiate(Flower, flowerSpawnPosition, transform.rotation) as GameObject;
+		_flower.AddComponent<Rigidbody>();
+		_flower.rigidbody.AddForce(flowerRigidBodySpeed);
+		_flower.rigidbody.useGravity = false;
 	}
 }
