7cd63c67775bfa7390b4f10124f559d9c7f526c6
kinifi <chrisfigueroa16@gmail.com>
2015-01-25 13:00:18 -0500

    button is gone

---
+++ Assets/Scripts/actionAnywhere.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class actionAnywhere : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.touchCount > 0 || Input.anyKeyDown || Input.GetMouseButtonDown(0))
+		{
+			Debug.Log("Loading HighScore Scene");
+			Application.LoadLevel("HighScores");
+
+		}
+
+	}
+}
